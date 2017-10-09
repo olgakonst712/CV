7ddd193cafdae822a4b9aeb9f0e8eead1456fcfc
olgakonst712 <olgakonstantinova712@gmail.com>
2017-10-09 16:00:08 +0200

    change form

--- mailform.php
+++ mailform.php
@@ -2,141 +2,40 @@
 
 
 
- <?php
-/*
-	********************************************************************************************
-	CONFIGURATION
-	********************************************************************************************
-*/
-// destinataire est votre adresse mail. Pour envoyer à plusieurs à la fois, séparez-les par une virgule
-$destinataire = 'olgakonstantinova712@gmail.com';
- 
-// copie ? (envoie une copie au visiteur)
-$copie = 'oui';
- 
-// Action du formulaire (si votre page a des paramètres dans l'URL)
-// si cette page est index.php?page=contact alors mettez index.php?page=contact
-// sinon, laissez vide
-$form_action = '';
- 
-// Messages de confirmation du mail
-$message_envoye = "Votre message nous est bien parvenu !";
-$message_non_envoye = "L'envoi du mail a échoué, veuillez réessayer SVP.";
- 
-// Message d'erreur du formulaire
-$message_formulaire_invalide = "Vérifiez que tous les champs soient bien remplis et que l'email soit sans erreur.";
- 
-/*
-	********************************************************************************************
-	FIN DE LA CONFIGURATION
-	********************************************************************************************
-*/
- 
-/*
- * cette fonction sert à nettoyer et enregistrer un texte
- */
-function Rec($text)
-{
-	$text = htmlspecialchars(trim($text), ENT_QUOTES);
-	if (1 === get_magic_quotes_gpc())
-	{
-		$text = stripslashes($text);
-	}
- 
-	$text = nl2br($text);
-	return $text;
-};
- 
-/*
- * Cette fonction sert à vérifier la syntaxe d'un email
- */
-function IsEmail($email)
-{
-	$value = preg_match('/^(?:[\w\!\#\$\%\&\'\*\+\-\/\=\?\^\`\{\|\}\~]+\.)*[\w\!\#\$\%\&\'\*\+\-\/\=\?\^\`\{\|\}\~]+@(?:(?:(?:[a-zA-Z0-9_](?:[a-zA-Z0-9_\-](?!\.)){0,61}[a-zA-Z0-9_-]?\.)+[a-zA-Z0-9_](?:[a-zA-Z0-9_\-](?!$)){0,61}[a-zA-Z0-9_]?)|(?:\[(?:(?:[01]?\d{1,2}|2[0-4]\d|25[0-5])\.){3}(?:[01]?\d{1,2}|2[0-4]\d|25[0-5])\]))$/', $email);
-	return (($value === 0) || ($value === false)) ? false : true;
+<?php
+/* Email du destinataire (mettez ici votre email) */
+$destinataire = "olgakonstantinova712@gmail.com";
+
+/* RÃ©cupÃ©ration */
+$nom = $_POST['nom'];
+$email = $_POST['email'];
+$objet = $_POST['subject'];
+$message = $_POST['message'];
+$header = "From: " . $_POST['email'];
+
+/* VÃ©rification */
+if ( $nom == "" ) {
+  echo "Veuillez indiquer le nom.";
+  $erreur = true;
 }
- 
-// formulaire envoyé, on récupère tous les champs.
-$nom     = (isset($_POST['nom']))     ? Rec($_POST['nom'])     : '';
-$email   = (isset($_POST['email']))   ? Rec($_POST['email'])   : '';
-$objet   = (isset($_POST['subject']))   ? Rec($_POST['subject'])   : '';
-$message = (isset($_POST['message'])) ? Rec($_POST['message']) : '';
- 
-// On va vérifier les variables et l'email ...
-$email = (IsEmail($email)) ? $email : ''; // soit l'email est vide si erroné, soit il vaut l'email entré
-$err_formulaire = false; // sert pour remplir le formulaire en cas d'erreur si besoin
- 
-if (isset($_POST['envoi']))
-{
-	if (($nom != '') && ($email != '') && ($subject != '') && ($message != ''))
-	{
-		// les 4 variables sont remplies, on génère puis envoie le mail
-		$headers  = 'From:'.$nom.' <'.$email.'>' . "\r\n";
-		//$headers .= 'Reply-To: '.$email. "\r\n" ;
-		//$headers .= 'X-Mailer:PHP/'.phpversion();
- 
-		// envoyer une copie au visiteur ?
-		if ($copie == 'oui')
-		{
-			$cible = $destinataire.';'.$email;
-		}
-		else
-		{
-			$cible = $destinataire;
-		};
- 
-		// Remplacement de certains caractères spéciaux
-		$message = str_replace("&#039;","'",$message);
-		$message = str_replace("&#8217;","'",$message);
-		$message = str_replace("&quot;",'"',$message);
-		$message = str_replace('&lt;br&gt;','',$message);
-		$message = str_replace('&lt;br /&gt;','',$message);
-		$message = str_replace("&lt;","&lt;",$message);
-		$message = str_replace("&gt;","&gt;",$message);
-		$message = str_replace("&amp;","&",$message);
- 
-		// Envoi du mail
-		$num_emails = 0;
-		$tmp = explode(';', $cible);
-		foreach($tmp as $email_destinataire)
-		{
-			if (mail($email_destinataire, $subject, $message, $headers))
-				$num_emails++;
-		}
- 
-		if ((($copie == 'oui') && ($num_emails == 2)) || (($copie == 'non') && ($num_emails == 1)))
-		{
-			echo '<p>'.$message_envoye.'</p>';
-		}
-		else
-		{
-			echo '<p>'.$message_non_envoye.'</p>';
-		};
-	}
-	else
-	{
-		// une des 3 variables (ou plus) est vide ...
-		echo '<p>'.$message_formulaire_invalide.'</p>';
-		$err_formulaire = true;
-	};
-}; // fin du if (!isset($_POST['envoi']))
- 
-if (($err_formulaire) || (!isset($_POST['envoi'])))
-{
-	// afficher le formulaire
-	echo '
-	<form id="contact" method="post" action="'.$form_action.'">
-	<fieldset><legend>Vos coordonnées</legend>
-		<p><label for="nom">Nom :</label><input type="text" id="nom" name="nom" value="'.stripslashes($nom).'" tabindex="1" /></p>
-		<p><label for="email">Email :</label><input type="text" id="email" name="email" value="'.stripslashes($email).'" tabindex="2" /></p>
-	</fieldset>
- 
-	<fieldset><legend>Votre message :</legend>
-		<p><label for="objet">Objet :</label><input type="text" id="objet" name="objet" value="'.stripslashes($subject).'" tabindex="3" /></p>
-		<p><label for="message">Message :</label><textarea id="message" name="message" tabindex="4" cols="30" rows="8">'.stripslashes($message).'</textarea></p>
-	</fieldset>
- 
-	<div style="text-align:center;"><input type="submit" name="envoi" value="Envoyer le formulaire !" /></div>
-	</form>';
-};
+if ( $subject == "" ) {
+  echo "Veuillez indiquer le sujet.";
+  $erreur = true;
+}
+if ( $message == "" ) {
+  echo "Veuillez indiquer un message.";
+  $erreur = true;
+}
+
+/* VÃ©rification du mail */
+$email_regex = "^[_a-z0-9-]+(.[_a-z0-9-]+)*@[a-z0-9-]+.[a-z0-9-]";
+if ( $email == "" OR !eregi( $email_regex , $email ) ) {
+  echo "Veuillez indiquer un email valide.";
+  $erreur = true;
+}
+/* Envoi du mail */
+if ( !$erreur ) {
+  mail( $destinataire , $objet , $message , $header );
+  echo "Le message a Ã©tÃ© envoyÃ©.";
+}
 ?>
